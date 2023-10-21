c26772fa7eb03c00e721cdfcebae3c568b9e61eb
jhon botero <jhonbg7@gmail.com>
2023-10-21 15:36:25 -0500

    calculadora terminada

--- Laboritorio7.js
+++ Laboritorio7.js
@@ -5,6 +5,7 @@ function result()
     var valueText = textToOperate.value
     var number1
     var number2
+    var finalscore
     var valid
     valid = validateString(valueText)
     if(valid == true)
@@ -42,7 +43,8 @@ function result()
                             }
                             number1 = valueText.slice(i+1,j-1)
                             number2 = valueText.slice(k+2,i)
-                            valueText = parseInt(number1)*parseInt(number2)
+                            finalscore = parseFloat(number1)*parseFloat(number2)
+                            valueText = valueText.substring(0, k+2)+finalscore+valueText.substring(j-1, valueText.length)
                             break;
                         case "/":
                             var j = i+1
@@ -67,20 +69,85 @@ function result()
                             }
                             number1 = valueText.slice(k+2,i)
                             number2 = valueText.slice(i+1,j-1)
-                            valueText = parseInt(number1)/parseInt(number2)
+                            finalscore = parseFloat(number1)/parseFloat(number2)
+                            valueText = valueText.substring(0, k+2)+finalscore+valueText.substring(j-1, valueText.length)
                             break;        
                     }
-
-                    alert(k)
-                    alert(number1)
-                    alert(number2)
-                    alert(valueText)
                 }
                        
             }
             thereIsDOrM = thereIsDiOrMu(valueText)     
         }
-    }      
+        
+        var thereIsResult = thereAreNoSy(valueText)
+
+        while (thereIsResult != false) 
+        {
+            for(var i = 0; i < valueText.length;i++)
+            {
+                if(valueText[i] == "+" || valueText[i] == "-")
+                {
+                    switch(valueText[i])
+                    {
+                        case "+":
+                            var j = i+1
+                            var k = i-1
+                            var valid3 = false
+                            var valid4 = false
+                            while(valid3 == false)
+                            {
+                                if(valueText[j] == "+" ||valueText[j] == "-" || j == valueText.length)
+                                {
+                                    valid3 = true
+                                }
+                                j++
+                            }
+                            while(valid4 == false)
+                            {
+                                if(valueText[k] == "+" || valueText[k] == "-" || k == -1)
+                                {
+                                    valid4 = true
+                                }
+                                k--
+                            }
+                            number1 = valueText.slice(i+1,j-1)
+                            number2 = valueText.slice(k+2,i)
+                            finalscore = parseFloat(number1)+parseFloat(number2)
+                            valueText = valueText.substring(0, k+2)+finalscore+valueText.substring(j-1, valueText.length)
+                            break;
+                        case "-":
+                            var j = i+1
+                            var k = i-1
+                            var valid1 = false
+                            var valid2 = false
+                            while(valid1 == false)
+                            {
+                                if(valueText[j] == "+" || valueText[j] == "-" || j == valueText.length)
+                                {
+                                    valid1 = true
+                                }
+                                j++
+                            }
+                            while(valid2 == false)
+                            {
+                                if(valueText[k] == "+" || valueText[k] == "-" || k == -1)
+                                {
+                                    valid2 = true
+                                }
+                                k--
+                            }
+                            number1 = valueText.slice(k+2,i)
+                            number2 = valueText.slice(i+1,j-1)
+                            finalscore = parseFloat(number1)-parseFloat(number2)
+                            valueText = valueText.substring(0, k+2)+finalscore+valueText.substring(j-1, valueText.length)
+                            break;        
+                    }
+                    thereIsResult = thereAreNoSy(valueText)
+                }       
+            }     
+        }
+        textToOperate.value = valueText 
+    }     
 }
 
 function thereIsDiOrMu(word)
@@ -88,6 +155,18 @@ function thereIsDiOrMu(word)
     for (let index = 0; index < word.length; index++) 
     {
         if(word[index] == "*" || word[index] == "/")
+        {
+            return true
+        }   
+    }
+    return false
+}
+
+function thereAreNoSy(word)
+{
+    for (let index = 0; index < word.length; index++) 
+    {
+        if(word[index] == "+" || word[index] == "-")
         {
             return true
         }   
